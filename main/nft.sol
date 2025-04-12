a3071b79856fd3dcd6faad67b6e26d8a09ed5428
Amro-Bsata <amrnmr66@gmail.com>
2025-04-12 20:47:51 +0200

    owner

--- main/nft.sol
+++ main/nft.sol
@@ -3,14 +3,37 @@ pragma solidity 0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract Nft is ERC721 {
+
+contract OwnerNft is ERC721  {
+    address public owner;
+
+    constructor(string memory song_name) ERC721("OWNER_NFT",song_name){
+        owner = msg.sender;
+        super._mint(owner, 0);
+    }
+ 
+     modifier onlyOwner() {
+        require(msg.sender == owner, "only owners can mint");
+        _; // executes the contract code in the function body
+    }  
+
+    function transferOwnership(address new_owner) external onlyOwner{
+        super._transfer(owner, new_owner, 0);
+        owner = new_owner;
+    }
 
 
-        uint32 total_supply;
+}
+
+contract FanNft  is ERC721 {
+
+
+        uint32 public  total_supply;
         uint32 token_id;
-        uint32 max_supply;
-        uint32 available_supply;
+        uint32 public  max_supply;
+        uint32 public available_supply;
         uint8 max_token_per_address = 1;
+        bool lock = false;
 
 
     constructor(string memory song_name, uint32 _max_supply) ERC721("FAN_NFT", song_name) {
@@ -21,28 +44,49 @@ contract Nft is ERC721 {
 
 
     modifier checkSupply(address user){
+        require(!lock,"sorry someone else is minting now, please try again later");
+        lock = true; // to avoid security bugs
         require (available_supply > 0, "sorry not enough supply");
         bool has_minted = super.balanceOf(user) > 0 ? true : false ;
         require(has_minted == false , "you have already minted");
         _;
+        lock = false;
     }
 
-    function mintToken()  external checkSupply(msg.sender){
+    function mintToken()  external {
         mintToken(msg.sender);
         
     }
 
     function mintToken(address _to) public checkSupply(_to){
-        uint32 tokenId = token_id; //save the value for current id
+        super._safeMint(_to, token_id);
         token_id+=1;
         available_supply -= 1;
         total_supply += 1;
-        super._safeMint(_to, tokenId);
     }
     
     
-
-
-
-
 }
+
+contract NftFactory is OwnerNft , FanNft {
+string public song_name;
+uint32 maxSupply =0 ;
+
+constructor(string memory _song_name, uint32 supply) 
+    OwnerNft(_song_name)
+   FanNft(_song_name,supply){
+      song_name=_song_name;
+
+maxSupply=supply; // initialize the variable
+}
+ 
+
+function newOwner(address owner_address ) external {
+  transferOwnership(owner_address);
+}
+
+ function getFanToken() public returns (uint256) { 
+ return token_id;
+ }
+
+ }
